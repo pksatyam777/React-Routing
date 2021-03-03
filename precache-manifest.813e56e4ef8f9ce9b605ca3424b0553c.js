cf94cb840f4673ef0a9398ad9d28937ab9c64a45
pksatyam777 <pksatyam777@gmail.com>
2021-03-03 15:54:45 +0530

    Updates

---
+++ precache-manifest.813e56e4ef8f9ce9b605ca3424b0553c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4b2a24621e1bf34e468a9f5a0fd14d",
+    "url": "/React-Routing/index.html"
+  },
+  {
+    "revision": "d3cf3c6298211b4a02fd",
+    "url": "/React-Routing/static/css/main.bddcc0bb.chunk.css"
+  },
+  {
+    "revision": "79a6776187946f472cc6",
+    "url": "/React-Routing/static/js/2.41aa7fad.chunk.js"
+  },
+  {
+    "revision": "28f29a598e465553cfeea810767399d3",
+    "url": "/React-Routing/static/js/2.41aa7fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3cf3c6298211b4a02fd",
+    "url": "/React-Routing/static/js/main.4d6426c5.chunk.js"
+  },
+  {
+    "revision": "112b060d5ba59891c585",
+    "url": "/React-Routing/static/js/runtime-main.34d3c287.js"
+  },
+  {
+    "revision": "2625c827c24ce877ebb0e6f1723c0afd",
+    "url": "/React-Routing/static/media/wall.2625c827.jpg"
+  }
+]);
